8e0bbe5749ef182c9c89ff1a31e630c6a80136d6
Бронислав <atlantwindow@gmail.com>
2025-05-17 15:16:54 +0300

    implement filtering, ordering, and pagination to BookViewSet

--- books/views.py
+++ books/views.py
@@ -4,6 +4,7 @@ from rest_framework import viewsets
 from books.models import Book
 from books.serializers import BookSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 from books.filters import BookFilter
 
 
@@ -11,7 +12,7 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_class = BookFilter
 
     ordering_fields = ["title", "daily_fee", "inventory", "author"]
