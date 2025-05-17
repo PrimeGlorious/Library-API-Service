a9a8a141e17eba8baec24a442367e10b7e464f91
PrimeGlorious <TheComboPunk@gmail.com>
2025-05-18 00:44:00 +0300

    Add owner validation for borrowings

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,6 +1,9 @@
 from django.utils import timezone
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import mixins, status
 from rest_framework.decorators import action
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.filters import OrderingFilter
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
@@ -8,7 +11,9 @@ from rest_framework.viewsets import GenericViewSet
 from borrowings.models import Borrowing
 from borrowings.serializers import (
     BorrowingDetailSerializer,
-    BorrowingSerializer, BorrowingListSerializer, BorrowingEmptySerializer
+    BorrowingSerializer,
+    BorrowingListSerializer,
+    BorrowingEmptySerializer
 )
 
 
@@ -20,6 +25,12 @@ class BorrowingsViewSet(
 ):
     queryset = Borrowing.objects.all().order_by("-borrow_date")
     permission_classes = (IsAuthenticated,)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.user != request.user:
+            raise PermissionDenied("You do not have permission to view this borrowing.")
+        return super().retrieve(request, *args, **kwargs)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
@@ -42,6 +53,12 @@ class BorrowingsViewSet(
     @action(detail=True, methods=["post"], name="book_return", url_path="return")
     def book_return(self, request, pk=None):
         borrowing = self.get_object()
+
+        if request.user != borrowing.user:
+            return Response(
+                {"detail": "You are not the owner of this book"},
+                status=status.HTTP_403_FORBIDDEN
+            )
 
         if borrowing.actual_return_date is not None:
             return Response(
