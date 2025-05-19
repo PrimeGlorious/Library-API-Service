886a42dc5b95c03495c00bb2a1b617fd38e0a01f
Denys Dikhtiar <dinis.dihtjar08@gmail.com>
2025-05-19 21:51:20 +0300

    add sign up view, and virify email view. Add swagger doc to this method

--- user/views.py
+++ user/views.py
@@ -1,12 +1,17 @@
-from rest_framework import generics
+import jwt
+from django.contrib.auth import get_user_model
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import generics, status, response
 from rest_framework.authentication import SessionAuthentication
+from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
 
-from user.serializers import UserSerializer
-
-
-class CreateUserView(generics.CreateAPIView):
-    serializer_class = UserSerializer
+from user.serializers import UserSerializer, EmailVerificationSerializer
+from user.utils import Util
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
@@ -18,3 +23,88 @@ class ManageUserView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+
+
+class SignUp(GenericAPIView):
+    serializer_class = UserSerializer
+
+    def post(self, request):
+        data = request.data
+        serializer = self.serializer_class(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        user = serializer.data
+
+        # getting tokens
+        user_email = get_user_model().objects.get(email=user["email"])
+        tokens = RefreshToken.for_user(user_email).access_token
+        # send email for user verification
+        current_site = get_current_site(request).domain
+        relative_link = reverse("user:email-verify")
+        absurl = "http://" + current_site + relative_link + "?token=" + str(tokens)
+        email_body = (
+            f"Hi {user_email} Use the link below to verify your email \n" + absurl
+        )
+        data = {
+            "email_body": email_body,
+            "to_email": user["email"],
+            "email_subject": "Verify your email",
+        }
+
+        Util.send_email(data=data)
+
+        return response.Response({"user_data": user}, status=status.HTTP_201_CREATED)
+
+
+class VerifyEmail(GenericAPIView):
+    serializer_class = EmailVerificationSerializer
+
+    token_param_config = openapi.Parameter(
+        'token',
+        in_=openapi.IN_QUERY,
+        description='Email verification token',
+        type=openapi.TYPE_STRING,
+        required=True
+    )
+
+    @swagger_auto_schema(
+        manual_parameters=[token_param_config],
+        responses={
+            200: openapi.Response(
+                description="Email successfully verified",
+                examples={
+                    "application/json": {
+                        "email": "Successfully activated"
+                    }
+                }
+            ),
+            400: openapi.Response(
+                description="Invalid or expired token",
+                examples={
+                    "application/json": {
+                        "error": "Activation Expired"
+                    }
+                }
+            )
+        }
+    )
+    def get(self, request):
+        token = request.GET.get("token")
+        try:
+            payload = jwt.decode(token, options={"verify_signature": False})
+            print(payload)
+            user = get_user_model().objects.get(id=payload["user_id"])
+            if not user.is_verified:
+                user.is_verified = True
+                user.save()
+            return response.Response(
+                {"email": "Successfully activated"}, status=status.HTTP_200_OK
+            )
+        except jwt.ExpiredSignatureError as identifier:
+            return response.Response(
+                {"error": "Activation Expired"}, status=status.HTTP_400_BAD_REQUEST
+            )
+        except jwt.exceptions.DecodeError as identifier:
+            return response.Response(
+                {"error": "Invalid token"}, status=status.HTTP_400_BAD_REQUEST
+            )
