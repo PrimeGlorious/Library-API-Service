a27ef7cb5bb81295bd27cd2d771ec7e6dd5ee8f9
Claymore-AI <dimalishchuk2003@gmail.com>
2025-05-19 11:22:28 +0300

    create base permissions for users and admin

--- books/views.py
+++ books/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from rest_framework import viewsets
 from books.models import Book
+from books.permissions import IsAdminOrReadOnly
 from books.serializers import BookSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
@@ -12,8 +13,11 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+    permission_classes = [IsAdminOrReadOnly]
+
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_class = BookFilter
 
     ordering_fields = ["title", "daily_fee", "inventory", "author"]
     ordering = ["title"]
+
