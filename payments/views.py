d95a08e107eea5ad0569e5c8f9c9d83be9ee8d1a
PrimeGlorious <TheComboPunk@gmail.com>
2025-05-19 15:45:47 +0300

    Create success and cancel views for payments

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from payments.models import Payment
+
+
+class PaymentSuccessView(APIView):
+    def get(self, request):
+        session_id = request.query_params.get("session_id")
+        if not session_id:
+            return Response(
+                {"detail": "Session ID is required"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        payment = Payment.objects.filter(session_id=session_id).first()
+        if not payment:
+            return Response(
+                {"detail": "Payment session not found"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        payment.status = Payment.Status.PAID
+        payment.save()
+
+        return Response({"detail": "Payment completed successfully"})
+
+class PaymentCancelView(APIView):
+    def get(self, request):
+        return Response({"detail": "Payment was cancelled. "
+                                   "You can pay later within 24 hours."})
