c68a6ad7c1bb040dd10075b5411f5301e7db317e
PrimeGlorious <TheComboPunk@gmail.com>
2025-05-19 15:29:46 +0300

    Add create_stripe_payment_session util

---
+++ payments/stripe_utils.py
@@ -0,0 +1,47 @@
+import stripe
+from django.conf import settings
+from decimal import Decimal
+from django.urls import reverse
+
+from payments.models import Payment
+
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+FINE_MULTIPLIER = 2
+
+def create_stripe_payment_session(borrowing, request, is_fine=False, overdue_days=0):
+    if is_fine:
+        total_amount = Decimal(overdue_days) * borrowing.book.daily_fee * FINE_MULTIPLIER
+        payment_type = "FINE"
+    else:
+        total_days = (borrowing.expected_return_date - borrowing.borrow_date).days
+        total_amount = borrowing.book.daily_fee * Decimal(total_days)
+        payment_type = "PAYMENT"
+
+    session = stripe.checkout.Session.create(
+        payment_method_types=["card"],
+        line_items=[{
+            "price_data": {
+                "currency": "usd",
+                "unit_amount": int(total_amount * 100),
+                "product_data": {
+                    "name": f"Payment for Borrowing ID {borrowing.id}",
+                },
+            },
+            "quantity": 1,
+        }],
+        mode="payment",
+        success_url=request.build_absolute_uri(reverse("payments:success")),
+        cancel_url=request.build_absolute_uri(reverse("payments:cancel")),
+    )
+
+    payment = Payment.objects.create(
+        borrowing=borrowing,
+        session_url=session.url,
+        session_id=session.id,
+        money_to_pay=total_amount,
+        type=payment_type
+    )
+
+    return payment
